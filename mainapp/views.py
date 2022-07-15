9fcb5268b573ebac2eb6b69dbe3cfb9734c5d647
vlad <kingbingping@gmail.com>
2022-07-15 14:19:12 +0500

    отредактировал пути

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def details_product(request):
+    return render(request, 'mainapp/product_details.html')
