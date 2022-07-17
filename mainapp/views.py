206e6d5a061aedf108cfc3cea0c3aac12c37a08a
Vlad <kingbingping@gmail.com>
2022-07-17 18:45:12 +0500

    json menu

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+import json
+import os
+
+path_menu = os.path.join(os.path.abspath('fixture'), 'menu.json')
+
+with open(r'D:\happybaby\mainapp\fixture\menu.json', encoding='utf-8') as file:
+    menu = json.load(file)
+
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context=menu)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html',context=menu)
 
 
 def details_product(request):
-    return render(request, 'mainapp/product_details.html')
+    return render(request, 'mainapp/product_details.html',context=menu)
